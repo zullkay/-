d697b227dc1016ba02d0fb61db1290d1c58fcdc8
katsuya utada <utada4@gmail.com>
2014-04-24 10:17:16 +0900

    modified:   ../views.py

--- facebook/views.py
+++ facebook/views.py
@@ -1,7 +1,7 @@
 from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='home.mak')
-def index(request):
+def home(request):
     settings = request.registry.settings
     #print(settings)
     #print(request)
